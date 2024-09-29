7a73874fd4f4c1dd5e12ea4b148e93be9554fc0d
raafi <raafi@tirzok.com>
2024-09-29 16:40:12 +0600

    updated manifest file and description file

--- __manifest__.py
+++ __manifest__.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "daily_inventory_purchase_report",
+    'name': "Tirzok Daily Inventory Purchase Report",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        This report enables users to easily generate a consolidated report for the Sales, Purchases, and Inventory modules, providing comprehensive results in a streamlined manner.
+        """,
 
     'description': """
-        Long description of module's purpose
+        In this report, users can generate comprehensive data through the Sales module's reporting menu. The report provides detailed insights into stock levels, purchase quantities, sales volumes, and the previous day's stock quantities. This functionality is particularly useful when products are moved between warehouses, as it ensures accurate tracking of stock in and out.
+        Additionally, the report offers a variety of filtering options, allowing users to customize the output based on specific needs. You can select multiple warehouses for analysis and filter data by various time frames, including day-wise, month-wise, quarterly, and yearly reports. These flexible filters enhance the precision of inventory tracking and reporting, making it easier to manage stock levels across different locations.
+        Overall, the report simplifies complex data, offering users a clear and actionable view of inventory movement, helping them make informed decisions, and improving overall operational efficiency.
+
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Tirzok Private Limited",
+    "license":  "AGPL-3",
+    'website': "https://tirzok.com, https://tirzokerp.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'sales',
+    'version': '13.0',
 
+    "live_test_url": "https://www.youtube.com/watch?v=3o7KR6p4l7Q",
     # any module necessary for this one to work correctly
     'depends': ['base', 'report_xlsx', 'stock', 'sale'],
 
@@ -33,4 +38,6 @@
     'demo': [
         'demo/demo.xml',
     ],
+
+    "images":["static/description/Banner.png", "static/description/Icon.png"],
 }
